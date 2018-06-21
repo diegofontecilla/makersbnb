55fe290fa631fb738f18b431bb35ade500e70bb3
Daniel Ridgway <ridginald@gmail.com>
2018-06-21 14:47:03 +0100

    added start and end date cals/functions

--- spec/listing_spec.rb
+++ spec/listing_spec.rb
@@ -3,11 +3,12 @@ require 'listing'
 describe Listing do
   it 'create and retrieve a new listing' do
     User.create(name: "simone", email: "simone@mail.com", password: "mimo")
-    Listing.create("Lovely flat in London", "5", "My flatmate is a zombie!", "20180621", 1)
+    Listing.create("Lovely flat in London", "5", "My flatmate is a zombie!", "2018-06-21", "2018-06-25", 1)
     expect(Listing.all.last.listing_title).to eq("Lovely flat in London")
     expect(Listing.all.last.listing_price).to eq("5")
     expect(Listing.all.last.listing_description).to eq("My flatmate is a zombie!")
-    expect(Listing.all.last.listing_available_dates).to eq("20180621")
+    expect(Listing.all.last.listing_start_date).to eq("2018-06-21")
+    expect(Listing.all.last.listing_end_date).to eq("2018-06-25")
     expect(Listing.all.last.listing_owner).to eq("simone")
   end
 end
